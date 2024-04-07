20d004b9e3b4cc3ff9826a22441f074eec37b78b
Bastian <bastiantobar.94@gmail.com>
2024-04-07 18:03:04 +0200

    Green

---
+++ src/main/java/v1/Interval.java
@@ -0,0 +1,13 @@
+package v1;
+
+public class Interval {
+
+	public Interval(double d, double e) {
+	}
+
+	public boolean include(double d) {
+		return true;
+	}
+
+
+}
---
+++ src/test/java/usantatecla/v1/IntervalTest.java
@@ -0,0 +1,14 @@
+package v1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class IntervalTest {
+  
+  @Test
+  public void givenIntervalwhenIncludeWithIncludedValueThenTrue(){
+    assertTrue(new Interval(-1.7,5555.0).include(0.0));
+  }
+  
+}
