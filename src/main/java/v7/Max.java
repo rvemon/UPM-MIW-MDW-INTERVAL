0b0db9adecd47a294becc25bfe013fe5e1482eb3
Bastian <bastiantobar.94@gmail.com>
2024-04-07 18:48:27 +0200

    refactor, red, add class Max

---
+++ src/main/java/v7/Max.java
@@ -0,0 +1,12 @@
+package v7;
+
+public class Max {
+
+    public Max(boolean b, int i) {
+    }
+
+    public boolean greater(double d) {
+        return false;
+    }
+
+}
--- src/test/java/v7/IntervalTest.java
+++ src/test/java/v7/IntervalTest.java
@@ -1,4 +1,4 @@
-package v6;
+package v5;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
