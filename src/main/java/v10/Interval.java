18932e8e8fd008b21bf83498475e73b518a7390f
ricardo.vente <ricardo751991@gmail.com>
2024-04-07 18:58:19 +0200

    Green

---
+++ src/main/java/v10/Interval.java
@@ -0,0 +1,24 @@
+package v10;
+
+public class Interval {
+
+	private double max;
+	private boolean maxOpen;
+
+	public Interval(double min, boolean maxOpen double max) {
+
+		this.maxOpen = maxOpen;
+		this.max = max;
+
+	}
+
+	public boolean include(double value) {
+
+		if (this.maxOpen){
+			return  value < this.max;
+		}
+		return value <= this.max;
+	}
+
+
+}
---
+++ src/main/java/v10/Max.java
@@ -0,0 +1,18 @@
+package v10;
+
+public class Max {
+    private double value;
+    private boolean open;
+    public Max(boolean b, int i) {
+        this.value = i;
+        this.open = b;
+    }
+
+    public boolean greater(double d) {
+        if (this.open){
+            return this.value > d;
+        }
+        return this.value >= d;
+    }
+
+}
