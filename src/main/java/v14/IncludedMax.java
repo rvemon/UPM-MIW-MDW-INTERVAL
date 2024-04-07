704de8c3468cf5d5e00fbea405db197cf80e3761
ricardo.vente <ricardo751991@gmail.com>
2024-04-07 19:41:45 +0200

    Refactor IncludeMax

---
+++ src/main/java/v14/IncludedMax.java
@@ -0,0 +1,11 @@
+package v14;
+public class IncludedMax {
+
+    public IncludedMax(int i, boolean b) {
+    }
+
+    public boolean greaterOrEquals(double value) {
+        return false;
+    }
+
+}
