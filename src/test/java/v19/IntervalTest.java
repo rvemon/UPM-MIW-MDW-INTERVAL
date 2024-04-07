536e2ae3cf642f30b2882541d9a6a91b0c0dcf86
Bastian <bastiantobar.94@gmail.com>
2024-04-07 20:24:48 +0200

    Red addIntervalTest use IncludeMax

---
+++ src/test/java/v19/IntervalTest.java
@@ -0,0 +1,36 @@
+package v14;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class IntervalTest {
+  
+  @Test
+  public void givenIntervaOpenOpenlwhenIncludeWithIncludedValueThenTrue(){
+    assertTrue(new Interval(true, -1.7, new Max(5555.0)).include(0.0));
+  }
+
+  @Test
+  public void givenIntervalOpenOpenwhenIncludeWithNotIncludedValueThenFalse(){
+    assertFalse(new Interval(true, -1.7, new Max(5555.0)).include(9000.0));
+  }
+  @Test
+  public void givenIntervalOpenOpenwhenIncludeWithLimitValueThenFalse(){
+    assertFalse(new Interval(true, -1.7, new Max(5555.0)).include(5555.0));
+  }
+  @Test
+  public void givenIntervalOpenClosewhenIncludeWithIncludedValueThenTrue(){
+    assertTrue(new Interval(true, -1.7, new IncludedMax(5555.0)).include(0.0));
+  }
+
+  @Test
+  public void givenIntervalOpenClosewhenIncludeWithNotIncludedValueThenFalse(){
+    assertFalse(new Interval(true, -1.7, new IncludedMax(5555.0)).include(9000.0));
+  }
+
+  @Test
+  public void givenIntervalOpenClosewhenIncludeWithLimitValueThenFalse(){
+    assertTrue(new Interval(true, -1.7, new IncludedMax(5555.0)).include(5555.0));
+  }
+}
