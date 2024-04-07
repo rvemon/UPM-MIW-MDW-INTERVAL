2b682f8923c3223fd30760b19cf34e5976fbdef2
ricardo.vente <ricardo751991@gmail.com>
2024-04-07 18:52:59 +0200

    Green

---
+++ src/test/java/v8/MaxTest.java
@@ -0,0 +1,15 @@
+
+package v8;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MaxTest {
+
+    @Test
+    public void givenIntervalwhenIncludeWithIncludedValueThenTrue(){
+        assertTrue(new Max(true, 4).greater(0.0));
+    }
+
+}
