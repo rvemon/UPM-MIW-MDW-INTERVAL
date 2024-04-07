f489073bf38ee1ab00a9c19d667289e1bd9446dd
Bastian <bastiantobar.94@gmail.com>
2024-04-07 19:13:33 +0200

    red MaxTest, IntervalTest

---
+++ src/test/java/v12/MaxTest.java
@@ -0,0 +1,40 @@
+
+package v11;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+import org.junit.jupiter.api.Test;
+
+public class MaxTest {
+
+    @Test
+    public void givenMaxOpenedwhenGreaterWithLessValueThenTrue(){
+        assertTrue(new Max(4, true).greaterOrEquals(0.0));
+    }
+
+    @Test
+    public void givenMaxOpenedwhenGreaterWithGreaterValueThenFalse(){
+        assertFalse(new Max(4, true).greaterOrEquals(5.0));
+    }
+
+    @Test
+    public void givenMaxOpenedwhenGreaterWithEqualValueThenFalse(){
+        assertFalse(new Max(4, true).greaterOrEquals(4));
+    }
+
+    @Test
+    public void givenMaxClosedwhenGreaterWithLessValueThenTrue(){
+        assertTrue(new Max(4, false).greaterOrEquals(0.0));
+    }
+
+    @Test
+    public void givenMaxClosedwhenGreaterWithGreaterValueThenFalse(){
+        assertFalse(new Max(4, false).greaterOrEquals(5.0));
+    }
+
+    @Test
+    public void givenMaxClosedwhenGreaterWithEqualValueThenFalse(){
+        assertTrue(new Max(4, false).greaterOrEquals(4));
+}
