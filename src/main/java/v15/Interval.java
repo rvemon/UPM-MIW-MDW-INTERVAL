2ff25bd398d621ddd33652f7c99a7dd2ab019f67
ricardo.vente <ricardo751991@gmail.com>
2024-04-07 20:10:12 +0200

    Solving version errors on Green

--- src/main/java/v15/Interval.java
+++ src/main/java/v15/Interval.java
@@ -7,17 +7,13 @@ public class Interval {
 	public Interval(Max max) {
 
 
-		this.maxOpen = maxOpen;
 		this.max = max;
 
 	}
 
 	public boolean include(double value) {
 
-		if (this.maxOpen){
-			return  value < this.max;
-		}
-		return value <= this.max;
+		this.max.greaterOrEquals(value);
 	}
 
 
--- src/main/java/v16/Interval.java
+++ src/main/java/v16/Interval.java
@@ -5,19 +5,13 @@ public class Interval {
 	private Max max;
 
 	public Interval(Max max) {
-
-
-		this.maxOpen = maxOpen;
 		this.max = max;
 
 	}
 
 	public boolean include(double value) {
 
-		if (this.maxOpen){
-			return  value < this.max;
-		}
-		return value <= this.max;
+		this.max.greaterOrEquals(value);
 	}
 
 
